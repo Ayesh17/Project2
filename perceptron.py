e6a5743d0516eaac217a7c8e5360a6107abeb135
Ayesh Buddhima <ayeshbuddima@gmail.com>
2022-10-14 12:20:14 -0700

    V10

--- perceptron.py
+++ perceptron.py
@@ -1,25 +1,50 @@
 import numpy as np
 
-
 def perceptron_train(X,Y):
     print(X)
     print(Y)
-    weights = np.zeros(len(X[0]))
-    bias=0
-    #do for 1D arrays too
-    for i in range(len(X)):
-        a=0
 
-        for j in range(len(X[0])):
-            a += X[i][j] * weights[j] + bias
-        if (a * Y[i] <= 0):
-            weights[j] = weights[j] + X[i][j]*Y[i]
-            bias = bias + Y[i]
-        print("iter", i, j)
-        print("a", a)
-        print("weights",weights)
-        print("bias",bias)
+    #do for 1D arrays too
+
+    weights = np.zeros(len(X[0]))
+    bias = 0
+    count,weights, bias = epoch(X,Y,weights, bias)
+    print(weights)
+    print(bias)
+
+    i=0
+    while(count !=0):
+        i+=1
+        print("epoch", i)
+        print()
+        count,weights, bias = epoch(X,Y,weights, bias)
+        print(weights)
+        print(bias)
 
 
     #a = wx+y
 
+def epoch(X,Y,weights,bias):
+
+    count = 0
+
+    for i in range(len(X)):
+        a = 0
+        for j in range(len(X[0])):
+            a += X[i][j] * weights[j]
+        a += bias
+        print("a",a)
+        if (a * Y[i] <= 0):
+            # print("count", count)
+            # print(Y[i])
+            count += 1
+            bias = bias + Y[i]
+            for k in range(len(X[0])):
+                weights[k] = weights[k] + X[i][k] * Y[i]
+
+        # print("iter", i, j)
+        # print("a", a)
+        # print("weights", weights)
+        # print("bias", bias)
+        # print("count2", count)
+    return count, weights, bias

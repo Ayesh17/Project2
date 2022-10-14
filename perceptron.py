67f16999cfe2f103f5e666adc4a9aaa4b9997b37
Ayesh Buddhima <ayeshbuddima@gmail.com>
2022-10-13 17:01:52 -0700

    V9

--- perceptron.py
+++ perceptron.py
@@ -9,17 +9,17 @@ def perceptron_train(X,Y):
     #do for 1D arrays too
     for i in range(len(X)):
         a=0
-        print(weights)
+
         for j in range(len(X[0])):
             a += X[i][j] * weights[j] + bias
         if (a * Y[i] <= 0):
             weights[j] = weights[j] + X[i][j]*Y[i]
             bias = bias + Y[i]
-        print("weights")
-        print(i)
-        print(weights)
-        print(bias)
-        print(a)
+        print("iter", i, j)
+        print("a", a)
+        print("weights",weights)
+        print("bias",bias)
+
 
     #a = wx+y
 

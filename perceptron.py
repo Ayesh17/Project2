a234f926cea379bbdb467853f72d290ad3757ea9
Ayesh Buddhima <ayeshbuddima@gmail.com>
2022-10-14 12:42:30 -0700

    V11

--- perceptron.py
+++ perceptron.py
@@ -15,6 +15,7 @@ def perceptron_train(X,Y):
     return weights,bias
     #a = wx+y
 
+
 def epoch(X,Y,weights,bias):
     count = 0
     for i in range(len(X)):
@@ -22,6 +23,7 @@ def epoch(X,Y,weights,bias):
         for j in range(len(X[0])):
             a += X[i][j] * weights[j]
         a += bias
+
         if (a * Y[i] <= 0):
             count += 1
             bias = bias + Y[i]
@@ -29,6 +31,29 @@ def epoch(X,Y,weights,bias):
                 weights[k] = weights[k] + X[i][k] * Y[i]
     return count, weights, bias
 
-def perceptron_test(X_test,Y_test,w,b):
 
-    return 0
+def perceptron_test(X_test,Y_test,w,b):
+    preds=[]
+    for i in range(len(X_test)):
+        a=0
+        for j in range(len(X_test[i])):
+            a += X_test[i][j]*w[j]
+        a+=b
+
+        if(a>=1):
+            a = 1
+        else:
+            a = -1
+        preds.append(a)
+
+    ##Calculate accuracy
+    correct =0
+    wrong =0
+    for i in range(len(Y_test)):
+        if preds[i] == Y_test[i]:
+            correct += 1
+        else:
+            wrong +=1
+    accuracy = correct / (correct + wrong)
+
+    return accuracy

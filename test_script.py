9858017447b532f6f8cefd2d9b2d3fa7dab7c620
Ayesh Buddhima <ayeshbuddima@gmail.com>
2022-10-14 20:01:56 -0700

    V15

--- test_script.py
+++ test_script.py
@@ -16,26 +16,26 @@ def load_data(file_data):
     Y = np.array(Y)
     return X,Y
 
-# X,Y = load_data("nearest_neighbors_3.csv")
-# acc = nn.KNN_test(X,Y,X,Y,1)
-# print("KNN:", acc)
-#
-# print(nn.choose_K(X,Y,X,Y))
+X,Y = load_data("nearest_neighbors_3.csv")
+acc = nn.KNN_test(X,Y,X,Y,1)
+print("KNN:", acc)
 
-# X = np.genfromtxt("clustering_2.csv", skip_header=1, delimiter=',')
-# #mu = np.array([[1],[5]])
-# mu = np.array([])
-# mu = kmeans.K_Means(X,3,mu)
-# print("KMeans:", mu)
-#
-# # plot2 = kmeans.plot2(X,mu)
-# mu = np.array([[0,0],[5,5], [10,10]])
-# plot3 = kmeans.plot3(X,mu)
-# # print(plot2)
-# print(plot3)
+print(nn.choose_K(X,Y,X,Y))
 
-X,Y = load_data("perceptron_3.csv")
-W = percept.perceptron_train(X,Y)
-acc = percept.perceptron_test(X,Y,W[0],W[1])
-print("Percept:", acc)
-plot = percept.plot(X,Y,W[0], W[1])
+X = np.genfromtxt("clustering_2.csv", skip_header=1, delimiter=',')
+#mu = np.array([[1],[5]])
+mu = np.array([])
+mu = kmeans.K_Means(X,3,mu)
+print("KMeans:", mu)
+
+# plot2 = kmeans.plot2(X,mu)
+
+plot3 = kmeans.plot3(X,mu)
+# print(plot2)
+print(plot3)
+
+# X,Y = load_data("perceptron_3.csv")
+# W = percept.perceptron_train(X,Y)
+# acc = percept.perceptron_test(X,Y,W[0],W[1])
+# print("Percept:", acc)
+# plot = percept.plot(X,Y,W[0], W[1])
